a9e43b60f1e874e98b1f1245b3d3cad28680e3d6
Mode <mode89@mail.ru>
2015-07-07 21:25:32 +0800

    esn : NetworkImpl : initialization of members

--- source/network.cpp
+++ source/network.cpp
@@ -3,7 +3,19 @@
 namespace ESN {
 
     NetworkImpl::NetworkImpl( unsigned neuronCount )
+        : mPotential( neuronCount )
+        , mThreshold( neuronCount )
+        , mResistance( neuronCount )
+        , mTimeConstant( neuronCount )
+        , mOutputCurrent( neuronCount )
+        , mConnection( neuronCount )
+        , mConnectionWeight( neuronCount )
     {
+        for ( auto & connection : mConnection )
+            connection.resize( neuronCount );
+
+        for ( auto & connectionWeight : mConnectionWeight )
+            connectionWeight.resize( neuronCount );
     }
 
     void NetworkImpl::Step( float step )
