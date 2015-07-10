c7f4f3846fbbfc1326000e10c1cc77e568e38d00
Mode <mode89@mail.ru>
2015-07-10 18:14:17 +0800

    esn : implemented function CreateNetwork(); instantiates class NetworkNSLI

--- esn/source/network_nsli.cpp
+++ esn/source/network_nsli.cpp
@@ -2,6 +2,12 @@
 
 namespace ESN {
 
+    std::unique_ptr< Network > CreateNetwork( unsigned neuronCount )
+    {
+        return std::unique_ptr< NetworkNSLI >(
+            new NetworkNSLI( neuronCount ) );
+    }
+
     NetworkNSLI::NetworkNSLI( unsigned neuronCount )
     {
     }
