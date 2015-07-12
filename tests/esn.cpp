bc5e0221ea94b2287a1f8489029c49988fb649c1
Mode <mode89@mail.ru>
2015-07-12 15:59:27 +0800

    tests : ESN:StepNSLI : initial implementation of the test

--- tests/esn.cpp
+++ tests/esn.cpp
@@ -10,3 +10,13 @@ TEST( ESN, CreateNetworkNSLI )
     params.outputCount = 10;
     std::unique_ptr< ESN::Network > network = CreateNetwork( params );
 }
+
+TEST( ESN, StepNSLI )
+{
+    ESN::NetworkParamsNSLI params;
+    params.inputCount = 11;
+    params.neuronCount = 100;
+    params.outputCount = 15;
+    std::unique_ptr< ESN::Network > network = CreateNetwork( params );
+    network->Step( 0.1f );
+}
