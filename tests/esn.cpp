09f35e2397039e5e885d4b12eb52675e96e07762
Mode <mode89@mail.ru>
2015-07-13 21:54:35 +0800

    tests : ESN:SetInputsNSLI : initial implementation of the test

--- tests/esn.cpp
+++ tests/esn.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <esn/create_network_nsli.h>
 #include <esn/network.h>
+#include <random>
 
 TEST( ESN, CreateNetworkNSLI )
 {
@@ -9,6 +10,21 @@ TEST( ESN, CreateNetworkNSLI )
     params.neuronCount = 100;
     params.outputCount = 10;
     std::unique_ptr< ESN::Network > network = CreateNetwork( params );
+}
+
+TEST( ESN, SetInputsNSLI )
+{
+    ESN::NetworkParamsNSLI params;
+    params.inputCount = 25;
+    params.neuronCount = 100;
+    params.outputCount = 17;
+    auto network = CreateNetwork( params );
+
+    std::default_random_engine randomEngine;
+    std::uniform_real_distribution<float > randomDist( -1.0f, 1.0f );
+    std::vector< float > inputs( params.inputCount );
+    for ( int i = 0; i < params.inputCount; ++ i )
+        inputs[i] = randomDist( randomEngine );
 }
 
 TEST( ESN, StepNSLI )
