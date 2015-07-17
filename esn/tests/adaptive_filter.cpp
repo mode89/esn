2c44bea89cc36009ec52c3527c02a4f24b4a4171
Mode <mode89@mail.ru>
2015-07-17 14:59:16 +0800

    esn : tests : AdaptiveFilter : LMS : generate inputs

--- esn/tests/adaptive_filter.cpp
+++ esn/tests/adaptive_filter.cpp
@@ -1,5 +1,26 @@
+#include <cmath>
+#include <Eigen/Dense>
 #include <gtest/gtest.h>
 
 TEST( AdaptiveFilter, LMS )
 {
+    const unsigned kInputCount = 10;
+    const unsigned kSampleCount = 100;
+    const float kMaxAmplitude = 1.0f;
+    const float kMaxFrequency = 10.0f;
+    const float kStep = 0.1f * 1.0f / kMaxFrequency;
+
+    ReferenceFilter referenceFilter( kInputCount );
+    Eigen::VectorXf input( kInputCount );
+    Eigen::VectorXf A = kMaxAmplitude / 2.0f *
+        ( Eigen::VectorXf::Random( kInputCount ).array() + 1.0f );
+    Eigen::VectorXf W = kMaxFrequency / 2.0f *
+        ( Eigen::VectorXf::Random( kInputCount ).array() + 1.0f );
+
+    for ( int i = 0; i < kSampleCount; ++ i )
+    {
+        float t = kStep * i;
+        input = A.array() * ( W.array() * t ).unaryExpr(
+            std::ptr_fun( std::sinf ) );
+    }
 }
