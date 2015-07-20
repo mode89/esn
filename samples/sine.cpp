0ac602ac8fd15ccc68b3283675fdfd681692fe49
Mode <mode89@mail.ru>
2015-07-20 16:26:41 +0800

    samples : sine : create network

--- samples/sine.cpp
+++ samples/sine.cpp
@@ -1,6 +1,14 @@
 #include <esn/esn.h>
 
+static const unsigned kNeuronCount = 100;
+
 int main()
 {
+    ESN::NetworkParamsNSLI params;
+    params.inputCount = 1;
+    params.neuronCount = kNeuronCount;
+    params.outputCount = 1;
+    std::unique_ptr< ESN::Network > network = ESN::CreateNetwork( params );
+
     return 0;
 }
