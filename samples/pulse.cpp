398ebc619ab8d7998f61cb2d25c558423ea0a674
Mode <mode89@mail.ru>
2015-07-23 17:12:22 +0800

    samples : pulse : define constant kNeuronCount = 100

--- samples/pulse.cpp
+++ samples/pulse.cpp
@@ -1,9 +1,11 @@
 #include <esn/esn.h>
 
+static const unsigned kNeuronCount = 100;
+
 int main()
 {
     ESN::NetworkParamsNSLI params;
-    params.neuronCount = 100;
+    params.neuronCount = kNeuronCount;
     params.inputCount = 1;
     params.outputCount = 1;
     auto network = ESN::CreateNetwork( params );
