eba72f6e3466d9f13827976d5096bbf03c71463f
Mode <mode89@mail.ru>
2015-07-23 19:03:34 +0800

    samples : pulse : generate reference output signal

--- samples/pulse.cpp
+++ samples/pulse.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <esn/esn.h>
 #include <iomanip>
 #include <iostream>
@@ -7,6 +8,18 @@ static const unsigned kNeuronCount = 100;
 static const float kStep = 0.01f;
 static const float kIntervalWidthMin = 0.1f;
 static const float kIntervalWidthMax = 1.0f;
+static const float kTargetIntervalWidth = 0.7f;
+static const float kTargetIntervalError = 0.1f;
+static const float kOutputPulseWidth = 0.1f;
+static const float kOutputPulseMax = 1.0f;
+static const float kOutputPulseThreshold = 0.0001f;
+
+inline float Pulse( float x, float width, float max )
+{
+    float retval = max * std::exp( -std::pow(
+        6.0f * ( x - width / 2.0f ) / width, 2.0f ) );
+    return ( retval < kOutputPulseThreshold ) ? 0.0f : retval;
+}
 
 int main()
 {
@@ -18,18 +31,39 @@ int main()
 
     bool inputState = false;
     std::vector< float > input( 1 );
+    float currentIntervalWidth = 0.0f;
     float nextInterval = 0.0f;
     std::default_random_engine randomEngine;
     std::uniform_real_distribution< float > random(
         kIntervalWidthMin, kIntervalWidthMax );
+
+    std::vector< float > referenceOutput( 1 );
+    float outputPulseStart = 0.0f;
+    float outputPulseAmplitude = 0.0f;
+
     for ( float time = 0.0f; true; time += kStep )
     {
         if ( time > nextInterval )
         {
+            if ( inputState &&
+                 std::fabs( currentIntervalWidth - kTargetIntervalWidth ) <
+                 kTargetIntervalError )
+            {
+                outputPulseStart = time;
+                outputPulseAmplitude = ( kTargetIntervalError - std::fabs(
+                    currentIntervalWidth - kTargetIntervalWidth ) ) /
+                    kTargetIntervalError;
+            }
+
             inputState = !inputState;
             input[0] = inputState ? 1.0f : 0.0f;
-            nextInterval += random( randomEngine );
+
+            currentIntervalWidth = random( randomEngine );
+            nextInterval += currentIntervalWidth;
         }
+
+        referenceOutput[0] = Pulse( time - outputPulseStart,
+            kOutputPulseWidth, outputPulseAmplitude );
 
         std::cout <<
             std::setw( 3 ) << inputState <<
