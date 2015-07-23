18420b5a7790d5f76e100ba5cf86dc6f83d22aa3
Mode <mode89@mail.ru>
2015-07-23 19:02:03 +0800

    samples : pulse : rename variables kIntervalMax -> kIntervalWidthMax, kIntervalMin -> kIntervalWidthMin

--- samples/pulse.cpp
+++ samples/pulse.cpp
@@ -5,8 +5,8 @@
 
 static const unsigned kNeuronCount = 100;
 static const float kStep = 0.01f;
-static const float kIntervalMin = 0.1f;
-static const float kIntervalMax = 1.0f;
+static const float kIntervalWidthMin = 0.1f;
+static const float kIntervalWidthMax = 1.0f;
 
 int main()
 {
@@ -21,7 +21,7 @@ int main()
     float nextInterval = 0.0f;
     std::default_random_engine randomEngine;
     std::uniform_real_distribution< float > random(
-        kIntervalMin, kIntervalMax );
+        kIntervalWidthMin, kIntervalWidthMax );
     for ( float time = 0.0f; true; time += kStep )
     {
         if ( time > nextInterval )
