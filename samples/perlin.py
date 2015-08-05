5e9ac3c8c39a09532b2da96ccdb16919548d7e76
Mode <mode89@mail.ru>
2015-08-05 13:54:53 +0800

    samples : python : perlin : refactoring
    Convert global methods to static methods of class Noise

--- samples/perlin.py
+++ samples/perlin.py
@@ -1,21 +1,12 @@
 import math
 
-def random( x ) :
-    x = ( x << 13 ) ^ x
-    return 1.0 - ( ( x * ( x * x * 15731 + 789221 ) + 1376312589 ) &
-        0x7fffffff ) / 1073741824.0
-
-def cosine_interpolate( a, b, x ) :
-    f = ( 1 - math.cos( x * math.pi ) ) * 0.5
-    return a * ( 1 - f ) + b * f
-
 class Noise :
 
     def __init__( self, persistence, octave_count ) :
         self.persistence = persistence
         self.octave_count = octave_count
 
-    def generate( self, x ) :
+    def __call__( self, x ) :
         retval = 0
         for octave in range( self.octave_count ) :
             frequency = 2 ** octave
@@ -26,6 +17,17 @@ class Noise :
     def _interpolated_random( self, x ) :
         x_int = int( x )
         x_fract = x - x_int
-        x1 = random( x_int )
-        x2 = random( x_int + 1 )
-        return cosine_interpolate( x1, x2, x_fract )
+        x1 = Noise._random( x_int )
+        x2 = Noise._random( x_int + 1 )
+        return Noise._cosine_interpolate( x1, x2, x_fract )
+
+    @staticmethod
+    def _random( x ) :
+        x = ( x << 13 ) ^ x
+        return 1.0 - ( ( x * ( x * x * 15731 + 789221 ) + 1376312589 ) &
+            0x7fffffff ) / 1073741824.0
+
+    @staticmethod
+    def _cosine_interpolate( a, b, x ) :
+        f = ( 1 - math.cos( x * math.pi ) ) * 0.5
+        return a * ( 1 - f ) + b * f
