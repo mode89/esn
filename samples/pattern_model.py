0bd85630b4558de0b5335041a48898bcac66e412
Mode <mode89@mail.ru>
2015-08-07 11:35:52 +0800

    samples : python : pattern : refactoring
    Remove underscore from names of member variables of class Model

--- samples/pattern_model.py
+++ samples/pattern_model.py
@@ -44,28 +44,28 @@ class Signal :
 class Model :
 
     def __init__( self, neuron_count ) :
-        self._network = esn.CreateNetworkNSLI(
+        self.network = esn.CreateNetworkNSLI(
                 inputCount=1,
                 neuronCount=neuron_count,
                 outputCount=1
             )
-        self._noise = perlin.Noise( persistence=0.5, octave_count=8 )
-        self._pattern = Signal( 1000 )
+        self.noise = perlin.Noise( persistence=0.5, octave_count=8 )
+        self.pattern = Signal( 1000 )
         self.time = 0
 
     def step( self, step ) :
-        self._pattern.step( step )
-        self.noise_value = self._noise( self.time ) * 0.3 + 0.5
-        self.input = self.noise_value + self._pattern.value
-        self._network.set_inputs( [ self.input ] )
-        self._network.step( step )
-        self.output = self._network.capture_output( 1 )[ 0 ]
+        self.pattern.step( step )
+        self.noise_value = self.noise( self.time ) * 0.3 + 0.5
+        self.input = self.noise_value + self.pattern.value
+        self.network.set_inputs( [ self.input ] )
+        self.network.step( step )
+        self.output = self.network.capture_output( 1 )[ 0 ]
 
         print( "%10s %10s %10s %10s" %
                 (
                     str( "%0.3f" % self.time ),
                     str( "%0.5f" % self.input ),
-                    str( "%0.5f" % self._pattern.value ),
+                    str( "%0.5f" % self.pattern.value ),
                     str( "%0.5f" % self.output )
                 )
             )
