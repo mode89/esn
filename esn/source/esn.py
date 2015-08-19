dd282e3a8de5931c0c8c6c518e0cfa835eb334ee
Mode <mode89@mail.ru>
2015-08-19 20:14:41 +0800

    esn : simplify search algorithm of shared library
    Use only ctypes.util.find_library() to find shared library from
    the python module.

--- esn/source/esn.py
+++ esn/source/esn.py
@@ -3,39 +3,7 @@ from ctypes.util import find_library
 import inspect
 import os
 
-# Search for ESN shared library
-
-_DLL_BASE_NAME = "esn"
-if os.name == "nt" :
-    _DLL_NAME = _DLL_BASE_NAME + ".dll"
-else :
-    _DLL_NAME = "lib" + _DLL_BASE_NAME + ".so"
-
-FIND_LIBRARY_PATHS = [
-    _DLL_BASE_NAME,
-    os.path.join( os.getcwd(), _DLL_BASE_NAME )
-]
-
-frames = inspect.stack()
-for frame in frames :
-    frameDir = os.path.dirname( frame[1] )
-    checkPath = os.path.join( frameDir, _DLL_BASE_NAME )
-    FIND_LIBRARY_PATHS.append( checkPath )
-
-for path in FIND_LIBRARY_PATHS :
-    _DLL_PATH = find_library( path )
-    if _DLL_PATH != None :
-        break
-    else :
-        pathDir = os.path.dirname( path )
-        checkPath = os.path.join( pathDir, _DLL_NAME )
-        if os.path.exists( checkPath ) :
-            _DLL_PATH = checkPath
-            break
-
-if _DLL_PATH == None :
-    raise RuntimeError( "Failed to find ESN shared library." )
-
+_DLL_PATH = find_library( "esn" )
 _DLL = cdll.LoadLibrary( _DLL_PATH )
 
 class Network :
@@ -117,6 +85,9 @@ def CreateNetworkNSLI(
     linearOutput = False,
     onlineTrainingForgettingFactor = 0.999,
     onlineTrainingInitialCovariance = 1000.0 ) :
+
+    if not _DLL._name :
+        raise RuntimeError( "ESN shared library hasn't been loaded." )
 
     params = NetworkParamsNSLI(
         structSize = sizeof( NetworkParamsNSLI ),
