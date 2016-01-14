560aa81feae90fcd202ff5871961a49fc9df0d99
Mode <mode89@mail.ru>
2016-01-14 22:46:33 +0800

    move eigen.cmake to cmake directory
    Keep all cmake files in cmake directory. Use first capital letter
    in the name of the file.

--- cmake/Eigen.cmake
+++ cmake/Eigen.cmake
@@ -1,6 +1,6 @@
-include( ExternalProject )
+include(ExternalProject)
 
-ExternalProject_Add( eigen
+ExternalProject_Add(eigen
     URL "http://bitbucket.org/eigen/eigen/get/3.2.5.tar.bz2"
     CONFIGURE_COMMAND echo Skip configuration
     BUILD_COMMAND echo Skip building
