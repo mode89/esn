49cb1c51f17de041feedf6a13397479a8c03b9f6
Mode <mode89@mail.ru>
2016-03-26 22:50:36 +0800

    add test-case ESN.TransformOutput
    Test output scaling and biasing.

--- tests/esn.cpp
+++ tests/esn.cpp
@@ -110,3 +110,45 @@ TEST(ESN, NoFeedback)
         network->TrainOnline(outputs, false);
     }
 }
+
+TEST(ESN, TransformOutput)
+{
+    ESN::NetworkParamsNSLI params;
+    params.inputCount = 32;
+    params.neuronCount = 64;
+    params.outputCount = 16;
+    auto network = CreateNetwork(params);
+
+    std::vector<float> outputs_min(params.outputCount);
+    Randomize(outputs_min, -10.0f, -1.0f);
+    std::vector<float> outputs_max(params.outputCount);
+    Randomize(outputs_max, 1.0f, 10.0f);
+
+    std::vector<float> scale(params.outputCount);
+    for (int i = 0; i < params.outputCount; ++i)
+        scale[i] = outputs_max[i] - outputs_min[i];
+    network->SetOutputScale(scale);
+
+    std::vector<float> bias(params.outputCount);
+    for (int i = 0; i < params.outputCount; ++i)
+        bias[i] = (outputs_max[i] + outputs_min[i]) / 2.0f;
+    network->SetOutputBias(bias);
+
+    std::vector<float> inputs(params.inputCount);
+    std::vector<float> outputs(params.outputCount);
+    for (int s = 0; s < 100; ++ s)
+    {
+        Randomize(inputs, -1.0f, 1.0f);
+        network->SetInputs(inputs);
+        network->Step(1.0f);
+
+        for (int i = 0; i < params.outputCount; ++i)
+        {
+            std::uniform_real_distribution<float> dist(
+                outputs_min[i], outputs_max[i]);
+            outputs[i] = dist(sRandomEngine);
+        }
+
+        network->TrainOnline(outputs, false);
+    }
+}
