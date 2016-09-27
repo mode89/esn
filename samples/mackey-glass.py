155d7ae42cc03fe440ca7c324bfc762d782e4444
Mode <mode89@mail.ru>
2016-09-27 23:35:00 +0800

    samples: train mackay-glass network

--- samples/mackey-glass.py
+++ samples/mackey-glass.py
@@ -4,12 +4,18 @@ from math import *
 from matplotlib import pyplot
 from matplotlib import animation
 
+NEURON_COUNT = 256
+CONNECTIVITY = 0.5
+WASHOUT_TIME = 1000
+TRAIN_TIME = 2000
+
 STEP = 0.1
-STEPS_PER_FRAME = 100
+STEPS_PER_FRAME = 10
+
 BETA = 2
 GAMMA = 1
 TAU = 2
-N = 9.65
+N = 9.0
 
 class MackayGlassSolver:
 
@@ -27,27 +33,60 @@ class MackayGlassSolver:
         return xt1 - 1.0
 
 if __name__ == "__main__":
+
+    print("Creating network ...")
+    params = esn.NetworkParamsNSLI()
+    params.inputCount = 1
+    params.neuronCount = NEURON_COUNT
+    params.outputCount = 1
+    params.connectivity = CONNECTIVITY
+    network = esn.CreateNetwork(params);
+
+    time = 0.0
+
+    print("Washing out ...")
+    while time < WASHOUT_TIME:
+        network.Step(STEP)
+        time = time + STEP
+
+    print("Training ...")
+    solver = MackayGlassSolver()
+    while time < TRAIN_TIME:
+        value = solver()
+        network.Step(STEP)
+        network.TrainOnline([value], True)
+        time = time + STEP
+
     figure = pyplot.figure()
     subplot = figure.add_subplot(111)
-    mgLine, = subplot.plot([], [])
+    mgLine, networkLine = subplot.plot([], [], [], [])
     subplot.set_xlim(-1, 1)
     subplot.set_ylim(-1.1, 1.1)
     subplot.grid(True)
 
     timeData = []
     mgData = []
-    outputData = []
-
-    time = 0.0
-    solver = MackayGlassSolver()
+    networkData = []
 
     def animationFunc(frame):
+
         global time
+
         for i in range(0, STEPS_PER_FRAME):
+
+            value = solver()
+
+            network.Step(STEP)
+            output = esn.Vector(1)
+            network.CaptureOutput(output)
+
             timeData.append(time)
-            mgData.append(solver())
+            mgData.append(value)
+            networkData.append(output[0])
             time = time + STEP
+
         mgLine.set_data(timeData, mgData)
+        networkLine.set_data(timeData, networkData)
         subplot.set_xlim(time - 100, time + 1)
 
     anim = animation.FuncAnimation(figure, animationFunc, interval=25)
