498c2239ba092cec78f9c9c8640c642d1e37a759
Mode <mode89@mail.ru>
2016-10-01 15:54:38 +0800

    network: train adaptive filter using CBLAS

--- source/adaptive_filter_rls.cpp
+++ source/adaptive_filter_rls.cpp
@@ -1,5 +1,9 @@
 #include <adaptive_filter_rls.h>
 
+extern "C" {
+#include <cblas/cblas.h>
+}
+
 namespace ESN {
 
     AdaptiveFilterRLS::AdaptiveFilterRLS( unsigned inputCount,
@@ -18,10 +22,30 @@ namespace ESN {
         float referenceOutput,
         const Eigen::VectorXf & input)
     {
-        mTemp = mP.transpose() * input;
-        mK = mP * input / (mForgettingFactor + mTemp.dot(input));
-        mP = 1 / mForgettingFactor * (mP - mK * mTemp.transpose());
-        w += ( referenceOutput - actualOutput ) * mK;
+        // mTemp = transpose(mP) * input
+        int N = input.size();
+        cblas_sgemv(CblasColMajor, CblasTrans, N, N, 1.0f, mP.data(), N,
+            input.data(), 1, 0.0f, mTemp.data(), 1);
+
+        // dot = mTemp * input
+        float dot = cblas_sdot(N, mTemp.data(), 1, input.data(), 1);
+
+        // mK = mP * input / (mForgettingFactor + dot)
+        cblas_sgemv(CblasColMajor, CblasNoTrans, N, N,
+            1.0f / (mForgettingFactor + dot), mP.data(), N,
+            input.data(), 1, 0.0f, mK.data(), 1);
+
+        // mP = 1 / mForgettingFactor * (mP - mK * mTemp.transpose())
+        // and BLAS representation
+        // mP = -1 / mForgettingFactor * mK * mTemp.transpose() +
+        //      1 / mForgettingFactor * mP
+        cblas_sgemm(CblasColMajor, CblasNoTrans, CblasTrans, N, N, 1,
+            -1 / mForgettingFactor, mK.data(), N, mTemp.data(), N,
+            1 / mForgettingFactor, mP.data(), N);
+
+        // w = (referenceOutput - actualOutput) * mK + w
+        cblas_saxpy(N, referenceOutput - actualOutput,
+            mK.data(), 1, w.data(), 1);
     }
 
 } // namespace ESN
