9dd5f254905d3c3de9ad7defacd1ed73515a9ec8
Mode <mode89@mail.ru>
2016-10-05 11:03:24 +0800

    cuda: copy cblas implementation

--- source/cuda/math.cpp
+++ source/cuda/math.cpp
@@ -1,9 +1,19 @@
 #include <cublas_v2.h>
+#include <esn/math.h>
 #include <memory>
+#include <random>
+
+extern "C" {
+    #include <cblas.h>
+}
+
+#include <lapacke.h>
 
 #define DEBUG(...) { printf(__VA_ARGS__); printf("\n"); }
 
 namespace ESN {
+
+    std::default_random_engine sRandomEngine;
 
     static cublasHandle_t & GetHandle()
     {
@@ -26,6 +36,111 @@ namespace ESN {
         }
 
         return *handle;
+    }
+
+    static inline CBLAS_TRANSPOSE ToCblasTranspose(char trans)
+    {
+        switch (trans)
+        {
+        case 'N':
+            return CblasNoTrans;
+        case 'T':
+            return CblasTrans;
+        }
+    }
+
+    static inline CBLAS_UPLO ToCblasUplo(char uplo)
+    {
+        switch (uplo)
+        {
+        case 'U':
+            return CblasUpper;
+        case 'L':
+            return CblasLower;
+        }
+    }
+
+    void RandomUniform(float * v, int size, float a, float b)
+    {
+        std::uniform_real_distribution<float> dist(a, b);
+        for (int i = 0; i < size; ++ i)
+            v[i] = dist(sRandomEngine);
+    }
+
+    void Constant(float * v, int size, float value)
+    {
+        for (int i = 0; i < size; ++ i)
+            v[i] = value;
+    }
+
+    void TanhEwise(float * v, int size)
+    {
+        for (int i = 0; i < size; ++ i)
+            v[i] = std::tanh(v[i]);
+    }
+
+    void ProductEwise(float * out, const float * in, int size)
+    {
+        for (int i = 0; i < size; ++ i)
+            out[i] *= in[i];
+    }
+
+    void SumEwise(float * out, const float * a, const float * b, int size)
+    {
+        for (int i = 0; i < size; ++ i)
+            out[i] = a[i] + b[i];
+    }
+
+    void SCOPY(const int n, const float * x, const int incx, float * y,
+        const int incy)
+    {
+        cblas_scopy(n, x, incx, y, incy);
+    }
+
+    void SAXPY(const int n, const float alpha, const float * x,
+        const int incx, float * y, const int incy)
+    {
+        cblas_saxpy(n, alpha, x, incx, y, incy);
+    }
+
+    float SDOT(const int n, const float * x, const int incx,
+        const float * y, const int incy)
+    {
+        return cblas_sdot(n, x, incx, y, incy);
+    }
+
+    void SGEMV(const char trans, const int m, const int n,
+        const float alpha, const float * a, const int lda, const float * x,
+        const int incx, const float beta, float * y, const int incy)
+    {
+        cblas_sgemv(CblasColMajor, ToCblasTranspose(trans), m, n, alpha,
+            a, lda, x, incx, beta, y, incy);
+    }
+
+    void SSBMV(const char uplo, const int n, const int k,
+        const float alpha, const float * a, const int lda, const float * x,
+        const int incx, const float beta, float * y, const int incy)
+    {
+        cblas_ssbmv(CblasColMajor, ToCblasUplo(uplo), n, k, alpha, a, lda,
+            x, incx, beta, y, incy);
+    }
+
+    void SGEMM(const char transa, const char transb, const int m,
+        const int n, const int k, const float alpha, const float * a,
+        const int lda, const float * b, const int ldb, const float beta,
+        float * c, const int ldc)
+    {
+        cblas_sgemm(CblasColMajor, ToCblasTranspose(transa),
+            ToCblasTranspose(transb), m, n, k, alpha, a, lda, b, ldb,
+            beta, c, ldc);
+    }
+
+    int SGESDD(const char jobz, const int m, const int n, float * a,
+        const int lda, float * s, float * u, const int ldu, float * vt,
+        const int ldvt)
+    {
+        return LAPACKE_sgesdd(LAPACK_COL_MAJOR, jobz, m, n, a, lda, s, u,
+            ldu, vt, ldvt);
     }
 
 } // namespace ESN
