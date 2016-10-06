d02d46225792b7062ab7d44938de02f645cb7644
Mode <mode89@mail.ru>
2016-10-06 11:02:08 +0800

    cuda: make_pointer() and memcpy() for float and std::vector

--- source/esn/cuda/pointer.cpp
+++ source/esn/cuda/pointer.cpp
@@ -4,7 +4,7 @@
 
 namespace ESN {
 
-    pointer make_pointer(int size)
+    pointer make_pointer(std::size_t size)
     {
         float * devicePointer = nullptr;
         VCU(cudaMalloc, &devicePointer, size);
@@ -13,14 +13,50 @@ namespace ESN {
         });
     }
 
-    void memcpy(const pointer & dst, const float * src, int byteSize)
+    pointer make_pointer(const float & value)
+    {
+        pointer retval = make_pointer(sizeof(float));
+        memcpy(retval, value);
+        return retval;
+    }
+
+    pointer make_pointer(const std::vector<float> & value)
+    {
+        pointer retval = make_pointer(value.size() * sizeof(float));
+        memcpy(retval, value);
+        return retval;
+    }
+
+    void memcpy(const pointer & dst, const float * src,
+        std::size_t byteSize)
     {
         VCU(cudaMemcpy, dst.get(), src, byteSize, cudaMemcpyHostToDevice);
     }
 
-    void memcpy(float * dst, const const_pointer & src, int byteSize)
+    void memcpy(float * dst, const const_pointer & src,
+        std::size_t byteSize)
     {
         VCU(cudaMemcpy, dst, src.get(), byteSize, cudaMemcpyDeviceToHost);
+    }
+
+    void memcpy(const pointer & dst, const float & src)
+    {
+        memcpy(dst, &src, sizeof(float));
+    }
+
+    void memcpy(float & dst, const const_pointer & src)
+    {
+        memcpy(&dst, src, sizeof(float));
+    }
+
+    void memcpy(const pointer & dst, const std::vector<float> & src)
+    {
+        memcpy(dst, src.data(), src.size() * sizeof(float));
+    }
+
+    void memcpy(std::vector<float> & dst, const const_pointer & src)
+    {
+        memcpy(dst.data(), src, dst.size() * sizeof(float));
     }
 
 } // namespace ESN
--- source/esn/pointer.h
+++ source/esn/pointer.h
@@ -2,15 +2,30 @@
 #define __ESN_POINTER_H__
 
 #include <memory>
+#include <vector>
 
 namespace ESN {
 
     using pointer = std::shared_ptr<float>;
     using const_pointer = std::shared_ptr<const float>;
 
-    pointer make_pointer(int byteSize);
-    void memcpy(const pointer & dst, const float * src, int byteSize);
-    void memcpy(float * dst, const const_pointer & src, int byteSize);
+    pointer make_pointer(std::size_t byteSize);
+    pointer make_pointer(const float &);
+    pointer make_pointer(const std::vector<float> &);
+
+    // General copy
+    void memcpy(const pointer & dst, const float * src,
+        std::size_t byteSize);
+    void memcpy(float * dst, const const_pointer & src,
+        std::size_t byteSize);
+
+    // Copy between single float and pointer
+    void memcpy(const pointer & dst, const float & src);
+    void memcpy(float & dst, const const_pointer & src);
+
+    // Copy between std::vector and pointer
+    void memcpy(const pointer & dst, const std::vector<float> & src);
+    void memcpy(std::vector<float> & dst, const const_pointer & src);
 
 } // namespace ESN
 
